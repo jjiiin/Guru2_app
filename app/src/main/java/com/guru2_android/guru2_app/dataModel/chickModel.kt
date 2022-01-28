b1abada542485a0ef086d3022a27e8a33c6529d4
jjiiin <wjdwlin4051@gmail.com>
2022-01-28 14:38:19 +0900

    main화면 전체 완료

--- app/src/main/java/com/guru2_android/guru2_app/dataModel/chickModel.kt
+++ app/src/main/java/com/guru2_android/guru2_app/dataModel/chickModel.kt
@@ -3,6 +3,7 @@ package com.guru2_android.guru2_app.dataModel
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
+//병아리의 uid, nickname 저장하는 데이터모델
 @Parcelize
 data class chickModel(
     val name:String="",
--- app/src/main/java/com/guru2_android/guru2_app/dataModel/eggModel.kt
+++ app/src/main/java/com/guru2_android/guru2_app/dataModel/eggModel.kt
@@ -1,5 +1,6 @@
 package com.guru2_android.guru2_app.dataModel
 
+//egg 모델
 data class eggModel(
     val date:String="",
     val egg:String="",
