9df0367c7a75154f4fd821c082a70c1eac1c85a3
Seul <ldsk1113@hanmail.net>
2022-01-31 20:01:58 +0900

    퀘스트 인증할 떄 이미지가 없으면 cert가 업로드 되지 않는 오류 해결 (만들어두신 certModel을 최대한 써보려 했으나 수정하는 과정에서 certModel을 사용하면 message를 중복해서 저장하게 되어 각각 setValue 하였습니다)

--- app/src/main/java/com/guru2_android/guru2_app/MainActivity.kt
+++ app/src/main/java/com/guru2_android/guru2_app/MainActivity.kt
@@ -27,10 +27,7 @@ import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage
-import com.guru2_android.guru2_app.dataModel.certModel
-import com.guru2_android.guru2_app.dataModel.eggModel
-import com.guru2_android.guru2_app.dataModel.firmModel
-import com.guru2_android.guru2_app.dataModel.jobModel
+import com.guru2_android.guru2_app.dataModel.*
 import com.guru2_android.guru2_app.dateDecorator.Day1Decorator
 import com.guru2_android.guru2_app.dateDecorator.Day2Decorator
 import com.guru2_android.guru2_app.dateDecorator.Day3Decorator
@@ -45,12 +42,14 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
     //병아리 회원일 때,
-      private lateinit var auth: FirebaseAuth
-//    private lateinit var month:String
+    private lateinit var auth: FirebaseAuth
+
+    //    private lateinit var month:String
 //    private lateinit var day:String
-      private lateinit var dateText:String
-      private lateinit var dateChange:String
-//    private lateinit var selectedDay: CalendarDay
+    private lateinit var dateText: String
+    private lateinit var dateChange: String
+
+    //    private lateinit var selectedDay: CalendarDay
     private var currentEgg = "0"    // 현재 egg
     private val pickStorage = 1001
     var setImage: Boolean = false
@@ -58,12 +57,12 @@ class MainActivity : AppCompatActivity() {
     private var imageUri: Uri? = null
 
     lateinit var mDialogView: View
-    val dataModelList= mutableListOf<jobModel>()
+    val dataModelList = mutableListOf<jobModel>()
 
     //3단계 평가
-    val decoList1= mutableListOf<CalendarDay>()
-    val decoList2= mutableListOf<CalendarDay>()
-    val decoList3= mutableListOf<CalendarDay>()
+    val decoList1 = mutableListOf<CalendarDay>()
+    val decoList2 = mutableListOf<CalendarDay>()
+    val decoList3 = mutableListOf<CalendarDay>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -76,7 +75,7 @@ class MainActivity : AppCompatActivity() {
 //        var month_tmp=""
 //        var day_tmp=""
         //var count=1 닭에서 퀘스트 생성할 때 숫자 필요함.
-        auth= Firebase.auth
+        auth = Firebase.auth
         val database = Firebase.database
 
         val currentYear = startTimeCalendar.get(Calendar.YEAR)
@@ -85,7 +84,8 @@ class MainActivity : AppCompatActivity() {
         endTimeCalendar.set(Calendar.MONTH, currentMonth + 3)
 
         // 현재 egg 가져오기
-        FirebaseDatabase.getInstance().reference.child(auth.currentUser?.uid.toString()).child("egg").child("totalEgg")
+        FirebaseDatabase.getInstance().reference.child(auth.currentUser?.uid.toString())
+            .child("egg").child("totalEgg")
             .addValueEventListener(object :
                 ValueEventListener {
                 override fun onDataChange(snapshot: DataSnapshot) {
@@ -94,6 +94,7 @@ class MainActivity : AppCompatActivity() {
                         currentEgg = "0"
                     }
                 }
+
                 override fun onCancelled(error: DatabaseError) {
                 }
             })
@@ -112,51 +113,53 @@ class MainActivity : AppCompatActivity() {
             .commit()
 
         //리싸이클러뷰
-        val rv=findViewById<RecyclerView>(R.id.mainRV)
-        val rvAdapter=RVAdapter(dataModelList)
-        rv.adapter=rvAdapter
-        val layout= LinearLayoutManager(this)
-        rv.layoutManager=layout
+        val rv = findViewById<RecyclerView>(R.id.mainRV)
+        val rvAdapter = RVAdapter(dataModelList)
+        rv.adapter = rvAdapter
+        val layout = LinearLayoutManager(this)
+        rv.layoutManager = layout
         rv.setHasFixedSize(true)
 
-        dateText=dayParse(CalendarDay.today())
-        dateChange=dayCleanParse(CalendarDay.today())
+        dateText = dayParse(CalendarDay.today())
+        dateChange = dayCleanParse(CalendarDay.today())
         materialCalendar.selectedDate = CalendarDay.today()
 
         //오늘 날짜를 노랑 볼드체로 표시하기
-        val todayDecorator= TodayDecorator(this)
+        val todayDecorator = TodayDecorator(this)
         materialCalendar.addDecorators(todayDecorator)
         //오늘의 job 가져오기
-        val schRef=database.getReference(auth.currentUser?.uid.toString()).child(dateText).child("jobs")
-        schRef.addValueEventListener(object: ValueEventListener {
+        val schRef =
+            database.getReference(auth.currentUser?.uid.toString()).child(dateText).child("jobs")
+        schRef.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
                 rv.removeAllViewsInLayout()
                 dataModelList.clear()
                 //itemKeyList.clear()
-                for(DataModel in snapshot.children){
+                for (DataModel in snapshot.children) {
                     dataModelList.add(DataModel.getValue(jobModel::class.java)!!)
                     //itemKeyList.add(DataModel.key.toString())
                 }
                 rvAdapter.notifyDataSetChanged()
-                Log.d("DataModel",dataModelList.toString())
+                Log.d("DataModel", dataModelList.toString())
             }
+
             override fun onCancelled(error: DatabaseError) {
                 TODO("Not yet implemented")
             }
         })
 
         //날짜 별 3단계 평가 표시하기
-        val firmRef=database.getReference(auth.currentUser?.uid.toString()).child("firm")
-        firmRef.addValueEventListener(object: ValueEventListener {
+        val firmRef = database.getReference(auth.currentUser?.uid.toString()).child("firm")
+        firmRef.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
-                for(DataModel in snapshot.children){
-                    val item=DataModel.getValue(firmModel::class.java)
-                    val dateStr=item!!.date.replace(".","")
-                    val transFormat= SimpleDateFormat("yyyyMMdd")
-                    val date=transFormat.parse(dateStr!!)
-                    val calDay=CalendarDay.from(date)
-                    Log.d("decoList2",calDay.toString())
-                    if(item!!.firm !="") {
+                for (DataModel in snapshot.children) {
+                    val item = DataModel.getValue(firmModel::class.java)
+                    val dateStr = item!!.date.replace(".", "")
+                    val transFormat = SimpleDateFormat("yyyyMMdd")
+                    val date = transFormat.parse(dateStr!!)
+                    val calDay = CalendarDay.from(date)
+                    Log.d("decoList2", calDay.toString())
+                    if (item!!.firm != "") {
                         when (item!!.firm.toInt()) {
                             1 -> decoList1.add(calDay)
                             2 -> decoList2.add(calDay)
@@ -164,35 +167,38 @@ class MainActivity : AppCompatActivity() {
                         }
                     }
                 }
-                Log.d("decoList2_1",decoList1.toString())
-                val dayDeco1= Day1Decorator(decoList1)
-                val dayDeco2= Day2Decorator(decoList2)
-                val dayDeco3= Day3Decorator(decoList3)
-                materialCalendar.addDecorators(dayDeco1,dayDeco2,dayDeco3)
+                Log.d("decoList2_1", decoList1.toString())
+                val dayDeco1 = Day1Decorator(decoList1)
+                val dayDeco2 = Day2Decorator(decoList2)
+                val dayDeco3 = Day3Decorator(decoList3)
+                materialCalendar.addDecorators(dayDeco1, dayDeco2, dayDeco3)
             }
+
             override fun onCancelled(error: DatabaseError) {
                 TODO("Not yet implemented")
             }
         })
         //날짜 누르는 이벤트-날짜 누르면 그 날의 job을 리싸이클러뷰에 추가하기기
         materialCalendar.setOnDateChangedListener { widget, date, selected ->
-            dateText=dayParse(date)
-            dateChange=dayCleanParse(date)
+            dateText = dayParse(date)
+            dateChange = dayCleanParse(date)
             //파이어베이스로부터 job 가져오기
-            val schRef=database.getReference(auth.currentUser?.uid.toString()).child(dateText).child("jobs")
-            schRef.addValueEventListener(object: ValueEventListener {
+            val schRef = database.getReference(auth.currentUser?.uid.toString()).child(dateText)
+                .child("jobs")
+            schRef.addValueEventListener(object : ValueEventListener {
                 override fun onDataChange(snapshot: DataSnapshot) {
                     rv.removeAllViewsInLayout()
                     dataModelList.clear()
                     //itemModelList.clear()
-                    for(DataModel in snapshot.children){
-                        val item=DataModel.getValue(jobModel::class.java)
+                    for (DataModel in snapshot.children) {
+                        val item = DataModel.getValue(jobModel::class.java)
                         dataModelList.add(item!!)
                         //itemKeyList.add(DataModel.key.toString())
                     }
                     rvAdapter.notifyDataSetChanged()
                     //Log.d("DataModel",dataModelList.toString())
                 }
+
                 override fun onCancelled(error: DatabaseError) {
                     TODO("Not yet implemented")
                 }
@@ -200,54 +206,63 @@ class MainActivity : AppCompatActivity() {
 
         }
         //리싸이클러뷰 클릭이벤트 - 인증 화면 뜨고 값 받아오기
-        rvAdapter.setItemClickListener(object:RVAdapter.OnItemClickListener{
+        rvAdapter.setItemClickListener(object : RVAdapter.OnItemClickListener {
             @RequiresApi(Build.VERSION_CODES.O)
             override fun onClick(v: View, position: Int) {
                 // 클릭 시 이벤트 작성-alertDialog 만들기(인증 화면)
-                val item=dataModelList[position]
-                mDialogView = LayoutInflater.from(this@MainActivity).inflate(R.layout.cert_chicken,null)
-                val mBuilder= AlertDialog.Builder(this@MainActivity).setView(mDialogView)
-                val mAlertDialog=mBuilder.show()
+                val item = dataModelList[position]
+                mDialogView =
+                    LayoutInflater.from(this@MainActivity).inflate(R.layout.cert_chicken, null)
+                val mBuilder = AlertDialog.Builder(this@MainActivity).setView(mDialogView)
+                val mAlertDialog = mBuilder.show()
 
 
-                val title=mAlertDialog.findViewById<TextView>(R.id.title)
-                val message=mAlertDialog.findViewById<EditText>(R.id.message)
-                val picBtn=mAlertDialog.findViewById<ImageView>(R.id.imgplus)
+                val title = mAlertDialog.findViewById<TextView>(R.id.title)
+                val message = mAlertDialog.findViewById<EditText>(R.id.message)
+                val picBtn = mAlertDialog.findViewById<ImageView>(R.id.imgplus)
                 //val img=mAlertDialog.findViewById<ImageView>(R.id.content)
-                val saveBtn=mAlertDialog.findViewById<TextView>(R.id.saveBtn)
+                val saveBtn = mAlertDialog.findViewById<TextView>(R.id.saveBtn)
 
-                title?.text=item.title
+                title?.text = item.title
                 picBtn?.setOnClickListener {
                     pickImage()
                 }
                 saveBtn?.setOnClickListener {
                     val certDatabase =
-                        database.getReference(auth.currentUser?.uid.toString()).child(dateText).child("jobs").child(item.title).child("cert")
+                        database.getReference(auth.currentUser?.uid.toString()).child(dateText)
+                            .child("jobs").child(item.title).child("cert")
 
                     if (setImage == true) {
-                        FirebaseStorage.getInstance().reference.child(auth.currentUser?.uid.toString()).child(dateText).child("jobs")
+                        FirebaseStorage.getInstance().reference.child(auth.currentUser?.uid.toString())
+                            .child(dateText).child("jobs")
                             .child(item.title).child("cert").child("image")
                             .putFile(imageUri!!)
                             .addOnSuccessListener {
-                                FirebaseStorage.getInstance().reference.child(auth.currentUser?.uid.toString()).child(dateText).child("jobs").child(item.title).child("cert").child("image").downloadUrl.addOnSuccessListener {
+                                FirebaseStorage.getInstance().reference.child(auth.currentUser?.uid.toString())
+                                    .child(dateText).child("jobs").child(item.title).child("cert")
+                                    .child("image").downloadUrl.addOnSuccessListener {
                                         questPicture = it
                                         Log.d("tag", "$questPicture")
-                                        val model=certModel(questPicture.toString(),message?.text.toString())
-                                        certDatabase.setValue(model)
+                                        certDatabase.child("image")
+                                            .setValue(questPicture.toString())
+//                                    val model =
+//                                        certModel(questPicture.toString(), message?.text.toString())
+//                                    certDatabase.setValue(model)
                                     }
                             }
                     }
 
-                    //certDatabase.child("message").setValue(message?.text.toString())
-
                     //완료 done 키값 바꾸기-색상 변경됨.(RVAdapter)
-                    val model=jobModel(item.title,item.sub,item.time,item.image,"1",item.egg)
-                    val schRef=database.getReference(auth.currentUser?.uid.toString()).child(dateText).child("jobs")
+                    val model = jobModel(item.title, item.sub, item.time, item.image, "1", item.egg)
+                    val schRef =
+                        database.getReference(auth.currentUser?.uid.toString()).child(dateText)
+                            .child("jobs")
                     schRef.child(item.title).setValue(model)
 
                     //퀘스트 수행 완료 egg내역에 추가(위에랑 같이 움직이기)
-                    val eggRef=database.getReference(auth.currentUser?.uid.toString()).child("egg")
-                    val eggModel= eggModel(dateChange,item.egg,item.title)
+                    val eggRef =
+                        database.getReference(auth.currentUser?.uid.toString()).child("egg")
+                    val eggModel = eggModel(dateChange, item.egg, item.title)
                     eggRef.push().setValue(eggModel)
 
                     // 퀘스트 수행 완료시 egg 추가
@@ -256,6 +271,9 @@ class MainActivity : AppCompatActivity() {
                     Log.d("tag", "item.egg : ${item.egg}")
                     eggRef.child("totalEgg").child("egg").setValue(changeEgg.toString())
 
+                    // 이미지가 없을 때 메세지 저장
+                    certDatabase.child("message").setValue(message?.text.toString())
+
                     mAlertDialog.dismiss()
                     //setResult(RESULT_OK)
                 }
@@ -263,46 +281,51 @@ class MainActivity : AppCompatActivity() {
         })
 
         //마이페이지
-        val myBtn=findViewById<ImageView>(R.id.my)
-         myBtn.setOnClickListener {
-             var intent = Intent(this, MypageActivity::class.java)
-             startActivity(intent)
+        val myBtn = findViewById<ImageView>(R.id.my)
+        myBtn.setOnClickListener {
+            var intent = Intent(this, MypageActivity::class.java)
+            startActivity(intent)
         }
 
     }
 
-    private fun dayParse(date:CalendarDay): String {
-        var month:String
-        var day:String
+    private fun dayParse(date: CalendarDay): String {
+        var month: String
+        var day: String
         //var dateText:String
         var selectedDay: CalendarDay
 
-        var DATE : String
-        var year : String
-        var month_tmp=""
-        var day_tmp=""
+        var DATE: String
+        var year: String
+        var month_tmp = ""
+        var day_tmp = ""
 
         selectedDay = date
-        DATE=selectedDay.toString()
+        DATE = selectedDay.toString()
         var parsedDATA: List<String> = date.toString().split("{")
         parsedDATA = parsedDATA[1].split("}").toList()
         parsedDATA = parsedDATA[0].split("-").toList()
-        year= parsedDATA[0].toInt().toString()
-        if(parsedDATA[1].toInt()<10){
-            var tmp=parsedDATA[1].toInt()+1
-            month_tmp="0$tmp"
-        }else{month_tmp=(parsedDATA[1].toInt()+1).toString()}
-        month=month_tmp
-        if(parsedDATA[2].toInt()<10){
-            var tmp=parsedDATA[2].toInt()+1
-            day_tmp="0$tmp"
-        }else{ day_tmp=parsedDATA[2].toInt().toString()}
-        day=day_tmp
+        year = parsedDATA[0].toInt().toString()
+        if (parsedDATA[1].toInt() < 10) {
+            var tmp = parsedDATA[1].toInt() + 1
+            month_tmp = "0$tmp"
+        } else {
+            month_tmp = (parsedDATA[1].toInt() + 1).toString()
+        }
+        month = month_tmp
+        if (parsedDATA[2].toInt() < 10) {
+            var tmp = parsedDATA[2].toInt() + 1
+            day_tmp = "0$tmp"
+        } else {
+            day_tmp = parsedDATA[2].toInt().toString()
+        }
+        day = day_tmp
         //Log.e("Date_DATE", DATE)
-        dateText="${year}${parsedDATA[1].toInt()+1}${parsedDATA[2].toInt()}"
-        DATE="${year}.${month}.${day}"//정제된 날짜
+        dateText = "${year}${parsedDATA[1].toInt() + 1}${parsedDATA[2].toInt()}"
+        DATE = "${year}.${month}.${day}"//정제된 날짜
         return dateText
     }
+
     private fun dayCleanParse(date: CalendarDay): String {
         var month: String
         var day: String
@@ -339,6 +362,7 @@ class MainActivity : AppCompatActivity() {
         DATE = "${year}.${month}.${day}"//정제된 날짜
         return DATE
     }
+
     // 이미지 불러오기
     private fun pickImage() {
         var intent = Intent(Intent.ACTION_PICK)
@@ -346,6 +370,7 @@ class MainActivity : AppCompatActivity() {
 
         startActivityForResult(intent, pickStorage)
     }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
